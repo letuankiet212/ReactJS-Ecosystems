60ee8e5341a13e90d4d0c108f1f6c5d11e30ef01
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2021-12-07 21:17:00 +0700

    up

--- server/server.js
+++ server/server.js
@@ -1,40 +1,13 @@
 const express = require('express');
+require('dotenv').config;
 const { uuid } = require('uuidv4');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
-
-app.use(cors());
 app.use(express.json({ extended: false }));
+app.use(cors());
 
-//Server tra ve` list danh sach cac viec can lam
-const listTodo = [
-  {
-    id: 1,
-    message: 'Di lam'
-  },
-  {
-    id: 2,
-    message: 'An com 2'
-  }
-];
-
-// Tao api xuat ra
-app.get('/api/list-todo', (req, res) => {
-  res.status(200).json(listTodo);
-});
-//Tao Api Tao todo
-app.post('/api/update-todo', (req, res) => {
-  const newTodo = {
-    id: uuid(),
-    message: req.body.message
-  };
-
-  listTodo.push(newTodo);
-  const result = { status: 1, listTodo: listTodo };
-  res.status(201).json(result);
-});
+//API
 
 const PORT = process.env.PORT || 5001;
 
